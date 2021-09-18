25a89847967174f6794871f2c34863ee7080b18d
Olivier Vadiavaloo <vadiaval@ualberta.ca>
2021-09-18 19:57:49 -0400

    Respond to GET for index.html and base.css

--- server.py
+++ server.py
@@ -26,21 +26,76 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+from http_req_parser import HttpReqParser
+from datetime import date
+from os.path import join
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    basepath = "./www"
+    index = join(basepath, "index.html")
+    basecss = join(basepath, "base.css")
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(4096).strip()
         # print ("Got a request of: %s\n" % self.data)
         # self.request.sendall(bytearray("OK",'utf-8'))
 
-        parsed_data = Parser.parse(self.data)
-        if parsed_data["request"] == "GET":
-            self.handle_get()
-        else:
-            print("Bad request!")
-            self.handle_bad_req()
+        res = self.template_res()
+
+        httpvername = HttpReqParser.httpvername
+        pathname = HttpReqParser.pathname
+        hostname = HttpReqParser.hostname
+        agentname = HttpReqParser.agentname
+        acceptname = HttpReqParser.accepname
+
+        try:
+            parsed_data = HttpReqParser.parse(self.data)
+            http_version = parsed_data[httpvername]
+            path = parsed_data[pathname]
+            host = parsed_data[hostname]
+            accept = parsed_data[acceptname]
+
+            if path == "/" or path == "/index.html":
+                index_f = open(self.index)
+                payload = index_f.read()
+
+                res = res.format(
+                    http_ver=http_version, 
+                    status=self.get_status_str(200), 
+                    content_type="text/html",
+                    payload=payload
+                )
+
+            if path == "/base.css":
+                css_f = open(self.basecss)
+                payload = css_f.readlines()
+
+                res = res.format(
+                    http_ver=http_version,
+                    status=self.get_status_str(200),
+                    content_type="text/css",
+                    payload=payload
+                )
+
+
+            self.request.sendall(bytearray(res, "utf-8"))
+
+        except Exception as e:
+            print(e.args)
     
+    def get_status_str(self, status_code):
+        if status_code == 200:
+            return "200 OK"
+    
+    def template_res(self):
+        res = "{http_ver} {status}\r\n"
+        res += "Server: localhost\r\n"
+        res += f"Date: {date.today()}\r\n"
+        res += "Content-Type: {content_type}\r\n\r\n"
+        res += "{payload}"
+        return res
+
     def handle_get(self):
         pass
 
